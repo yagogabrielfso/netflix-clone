576ccb61a773a43f037812fecf98d877ad6bd825
Yago <gabriel_oliveirafs@hotmail.com>
2022-11-17 16:28:50 -0300

    Project commit

---
+++ src/requests.jsx
@@ -0,0 +1,16 @@
+const API_KEY = "861b0d4c9fe108e4ae0393d4f1ff00d0";
+
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+    fetchActionMOvies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    
+}
+
+export default requests;
